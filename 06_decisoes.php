988414e29dc29da70c079e684f70023a235858e8
Ailton Xavier <ailton.xavier@hotmail.com.br>
2022-02-17 00:03:34 -0300

    Aula 5 completa

--- 06_decisoes.php
+++ 06_decisoes.php
@@ -1,17 +1,20 @@
 <?php
 
-$idade = 17;
+$idade = 18;
 $numeroDePessoas = 1;
 
-echo "Você só pode entrar se tiver mais que 18 anos.";
-echo "a partir de 16 acompanhado" . PHP_EOL;
+echo "Você só pode entrar se tiver mais que 18 anos ";
+echo "ou a partir de 16 acompanhado." . PHP_EOL;
 
 if ($idade >= 18){
   echo "Você tem $idade anos. Pode entrar" . PHP_EOL;
-  echo 'Pode entrar.';
-}
+  echo 'Pode entrar.'; }
+else if ($idade >= 16 && $numeroDePessoas > 1){
+    echo "Você tem $idade anos, está acompanhado(a), então pode entrar.";
+  }
 else {
  echo "Você só tem $idade anos, você não pode entrar!";
+ echo PHP_EOL . "É uma pena";
 }
 
 echo PHP_EOL . "Adeus";
